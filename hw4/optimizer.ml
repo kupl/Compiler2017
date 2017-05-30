5fc39a033201c79c77c8438ac7d0a3cea9a62637
오학주 <hakjoooh@ohagjuui-iMac.local>
2017-05-30 19:50:13 +0900

    hw4

---
+++ hw4/optimizer.ml
@@ -0,0 +1,4 @@
+(* TODO: Implement an optimizer *)
+
+let optimize : T.program -> T.program
+=fun t -> raise (Failure "optimizer: Not implemented")
---
+++ hw4/translator.ml
@@ -0,0 +1,4 @@
+(* TODO: Write a translator *)
+
+let translate : S.program -> T.program
+=fun s -> raise (Failure "translator: Not implemented")
